839021457162003db1cbdf8b6808bec499115c1b
Lea Jouvin <ljouvin@ccage014.in2p3.fr>
2014-11-17 17:41:13 +0100

    ca marche, maintenant il va falloir ajouté les commandes root pour la loi poissonienne

---
+++ check_fit.C
@@ -0,0 +1,73 @@
+// STL
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cassert>
+#include <cstdlib>
+
+//root
+#include "TROOT.h"
+#include "TMath.h"
+#include "TRandom.h"
+#include "TRandom3.h"
+#include "TSystem.h"
+#include "TDirectory.h"
+#include "Math/QuantFuncMathCore.h"
+#include "TUUID.h"
+
+// START
+#include "ComputeResults.hh"
+#include "Band.hh"
+#include "Hypothesis.hh"
+#include "BandsFactory.hh"
+#include "check_fit.hh"
+// Utilities
+#define DEBUG 0
+#include "debugging.hh"
+
+
+//Constructor
+START::CheckFit::CheckFit(BandsFactory &BandsFact ,std::vector<Band> &BandArray, int N_iteration, Hypothesis &hypo)
+{
+  //Initialize the N_On of the array to O
+  double n_on=0.;
+  BandsFact.Change_N_ON(BandArray,n_on);
+  fBandArray = BandArray;
+  // Define the number of point we want for the phi_O and the gamma reconstructed distribution
+  f_iteration= N_iteration;
+
+  //An object hypothesis that contains the parameter of the fit
+  fHypothesis = &hypo;
+
+  //An object Comput Result in order use the method FunctionExpectedExcess
+  fCompRes = new ComputeResults(BandArray,*fHypothesis);
+}
+
+START::CheckFit::~CheckFit()
+{
+}
+
+void START::CheckFit::add_N_On()
+ { 
+   double mean_signal, mean_background;
+   double signal, background;
+   double On_mean, On;
+
+
+  
+
+   for(unsigned int iband(0); iband<fBandArray.size(); iband++) {
+    
+     for(unsigned int ibin(0); ibin<fBandArray[iband].ebin.size(); ibin++) {
+       // functionexpectedexcess attend des pointeurs pour les bandes et l energie, il faut donc lui passer fbandArray par reference
+       mean_signal=fCompRes->FunctionExpectedExcess(&fBandArray[iband],&fBandArray[iband].ebin[ibin],fHypothesis->GetFittedParameters(), -1., -1.);
+       mean_background=fBandArray[iband].GetAlphaRun()*fBandArray[iband].ebin[ibin].GetOff();
+    //On_mean= mean_signal + mean_fond;
+    //ON = 
+    //fBandArray[iband].ebin[ibin].SetOn(ON)
+	
+     }
+
+     }
+   }
+//Ensuite faire un fit avec ces donnees. J ai pas compris si la fonction de minimizefactory ou on pouvait donner les donnees du fit a l avance nous sert la. Je pense que c etait peut etre que dans la class hypothesis on peut creeer un tableau qui nous sert vec les donnees gamma et phi 0 qui nous sert avant pour calculer l expected excess. 
