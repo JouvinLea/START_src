436ed037b206b3669035daf9ca3b33629934f3c5
Lea Jouvin <ljouvin@ccage021.in2p3.fr>
2014-11-18 11:59:00 +0100

    on utilise plus fabnd array et c est ok

--- check_fit.C
+++ check_fit.C
@@ -27,14 +27,12 @@
 
 
 //Constructor
-START::CheckFit::CheckFit(BandsFactory &BandsFact ,std::vector<Band> &BandArray, int N_iteration, Hypothesis &hypo)
+START::CheckFit::CheckFit(BandsFactory &BandsFact ,std::vector<Band> &BandArray, Hypothesis &hypo)
 {
   //Initialize the N_On of the array to O
   double n_on=0.;
   BandsFact.Change_N_ON(BandArray,n_on);
-  fBandArray = BandArray;
-  // Define the number of point we want for the phi_O and the gamma reconstructed distribution
-  f_iteration= N_iteration;
+  //fBandArray = BandArray;
 
   //An object hypothesis that contains the parameter of the fit
   fHypothesis = &hypo;
@@ -47,27 +45,27 @@ START::CheckFit::~CheckFit()
 {
 }
 
-void START::CheckFit::add_N_On()
+void START::CheckFit::add_N_On(std::vector<Band> &BandArray)
  { 
    double mean_signal, mean_background;
    double On_mean, On;
 
-   for(unsigned int iband(0); iband<fBandArray.size(); iband++) {
+   for(unsigned int iband(0); iband<BandArray.size(); iband++) {
     
-     for(unsigned int ibin(0); ibin<fBandArray[iband].ebin.size(); ibin++) {
+     for(unsigned int ibin(0); ibin<BandArray[iband].ebin.size(); ibin++) {
        // functionexpectedexcess attend des pointeurs pour les bandes et l energie, il faut donc lui passer fbandArray par reference
        //std:: cout << "phi0 =" << fHypothesis->GetFittedParameters()[0] << std::endl;
        //std:: cout << "gamma =" <<fHypothesis->GetFittedParameters()[1] << std::endl;
-       mean_signal=fCompRes->FunctionExpectedExcess(&fBandArray[iband],&fBandArray[iband].ebin[ibin],fHypothesis->GetFittedParameters(), -1, -1);
-       //std:: cout << "mean_signal = " << mean_signal << std::endl ;
-       mean_background=fBandArray[iband].GetAlphaRun()*fBandArray[iband].ebin[ibin].GetOff();
+       mean_signal=fCompRes->FunctionExpectedExcess(&BandArray[iband],&BandArray[iband].ebin[ibin],fHypothesis->GetFittedParameters(), -1, -1);
+       std:: cout << "mean_signal = " << mean_signal << std::endl ;
+       mean_background=BandArray[iband].GetAlphaRun()*BandArray[iband].ebin[ibin].GetOff();
        //std:: cout << "background OFF = " << fBandArray[iband].ebin[ibin].GetOff() << std::endl ;
-       //std:: cout << "mean_background = " << mean_background << std::endl ;
+       std:: cout << "mean_background = " << mean_background << std::endl ;
        On_mean= mean_signal + mean_background;
        TRandom trandom;
        On = trandom.Poisson(On_mean);
-       //std:: cout << "On = " << On << std::endl;
-       fBandArray[iband].ebin[ibin].SetOn(On);
+       std:: cout << "On = " << On << std::endl;
+       BandArray[iband].ebin[ibin].SetOn(On);
        
      }
      
