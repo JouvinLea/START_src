e9b8d855e5939d541bbfc6fd4d8b62385eed4b7e
Lea Jouvin <ljouvin@ccage026.in2p3.fr>
2014-11-18 09:15:35 +0100

    ca marche sauf qu'ensuite du a function expectedexcess j ai un segmentation fault a lexecution

--- check_fit.C
+++ check_fit.C
@@ -50,24 +50,27 @@ START::CheckFit::~CheckFit()
 void START::CheckFit::add_N_On()
  { 
    double mean_signal, mean_background;
-   double signal, background;
    double On_mean, On;
-
-
-  
 
    for(unsigned int iband(0); iband<fBandArray.size(); iband++) {
     
      for(unsigned int ibin(0); ibin<fBandArray[iband].ebin.size(); ibin++) {
        // functionexpectedexcess attend des pointeurs pour les bandes et l energie, il faut donc lui passer fbandArray par reference
-       mean_signal=fCompRes->FunctionExpectedExcess(&fBandArray[iband],&fBandArray[iband].ebin[ibin],fHypothesis->GetFittedParameters(), -1., -1.);
+       //std:: cout << "phi0 =" << fHypothesis->GetFittedParameters()[0] << std::endl;
+       //std:: cout << "gamma =" <<fHypothesis->GetFittedParameters()[1] << std::endl;
+       mean_signal=fCompRes->FunctionExpectedExcess(&fBandArray[iband],&fBandArray[iband].ebin[ibin],fHypothesis->GetFittedParameters(), -1, -1);
+       //std:: cout << "mean_signal = " << mean_signal << std::endl ;
        mean_background=fBandArray[iband].GetAlphaRun()*fBandArray[iband].ebin[ibin].GetOff();
-    //On_mean= mean_signal + mean_fond;
-    //ON = 
-    //fBandArray[iband].ebin[ibin].SetOn(ON)
-	
+       //std:: cout << "background OFF = " << fBandArray[iband].ebin[ibin].GetOff() << std::endl ;
+       //std:: cout << "mean_background = " << mean_background << std::endl ;
+       On_mean= mean_signal + mean_background;
+       TRandom trandom;
+       On = trandom.Poisson(On_mean);
+       //std:: cout << "On = " << On << std::endl;
+       fBandArray[iband].ebin[ibin].SetOn(On);
+       
      }
-
-     }
+     
    }
+ }
 //Ensuite faire un fit avec ces donnees. J ai pas compris si la fonction de minimizefactory ou on pouvait donner les donnees du fit a l avance nous sert la. Je pense que c etait peut etre que dans la class hypothesis on peut creeer un tableau qui nous sert vec les donnees gamma et phi 0 qui nous sert avant pour calculer l expected excess. 
