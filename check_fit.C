14cb8f87b2c38c31462ef437822eddcb126066d4
Lea Jouvin <ljouvin@ccage021.in2p3.fr>
2014-11-21 18:25:29 +0100

    t ok Please enter the commit message for your changes. Lines starting

--- check_fit.C
+++ check_fit.C
@@ -1,3 +1,4 @@
+
 // STL
 #include <iostream>
 #include <vector>
@@ -52,26 +53,29 @@ void START::CheckFit::add_N_On(std::vector<Band> &BandArray)
    double On_mean, On;
    //std:: cout << fHypothesis->GetFittedParameters()[0] <<std::endl;
    //std:: cout << fHypothesis->GetFittedParameters()[1] <<std::endl;
+   std:: cout << "Je suis dans add N ON" <<std::endl;
    for(unsigned int iband(0); iband<BandArray.size(); iband++) {
     
      for(unsigned int ibin(0); ibin<BandArray[iband].ebin.size(); ibin++) {
        // functionexpectedexcess attend des pointeurs pour les bandes et l energie, il faut donc lui passer fbandArray par reference
        //std:: cout << "phi0 =" << fHypothesis->GetFittedParameters()[0] << std::endl;
        //std:: cout << "gamma =" <<fHypothesis->GetFittedParameters()[1] << std::endl;
+       
+       
        mean_signal=fCompRes->FunctionExpectedExcess(&BandArray[iband],&BandArray[iband].ebin[ibin],fHypothesis->GetFittedParameters(), -1, -1);
-       //std:: cout << "mean_signal = " << mean_signal << std::endl ;
+     
        mean_background=BandArray[iband].GetAlphaRun()*BandArray[iband].ebin[ibin].GetOff();
-       //std:: cout << "background OFF = " << BandArray[iband].ebin[ibin].GetOff() << std::endl ;
-       //std:: cout << "alpha run = " << BandArray[iband].GetAlphaRun() << std::endl ;
-       //std:: cout << "mean_background = " << mean_background << std::endl ;
+       std:: cout << "jai passe mean background" <<std::endl;
+       ;
        On_mean= mean_signal + mean_background;
-       //std:: cout << "On_mean = " << On_mean << std::endl;
+       
        On = trandom.Poisson(On_mean);
-       //std:: cout << "On = " << On << std::endl;
+       
        BandArray[iband].ebin[ibin].SetOn(On);
        
      }
      
    }
+   
  }
 //Ensuite faire un fit avec ces donnees. J ai pas compris si la fonction de minimizefactory ou on pouvait donner les donnees du fit a l avance nous sert la. Je pense que c etait peut etre que dans la class hypothesis on peut creeer un tableau qui nous sert vec les donnees gamma et phi 0 qui nous sert avant pour calculer l expected excess. 
